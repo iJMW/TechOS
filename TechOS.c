de868219ea2b116b388b1ae07628b78a23dcd558
Jalen Wayt <jalen.wayt@gmail.com>
2021-09-04 16:16:11 -0400

    Added DisplayDate and ChangeDate functionality

--- TechOS.c
+++ TechOS.c
@@ -9,16 +9,25 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+time_t currentTime;
+struct tm tm;
 
 void COMHAN();
 void Help();
 void Version();
 void DisplayDate();
+char *getMonth(int month);
 void ChangeDate();
 void DisplayTime();
 void TerminateTechOS();
 
 int main(){
+    currentTime = time(NULL);
+    tm = *localtime(&currentTime);
+
     COMHAN();
 
     return 0;
@@ -26,18 +35,18 @@ int main(){
 
 void COMHAN(){
     char userInput[50];
-    while(strcmp(userInput, "Quit") != 0){
+    while(strcmp(userInput, "quit") != 0){
         printf("\nTechOS > ");
         scanf("%s", userInput);
         if(strcmp(userInput, "help") == 0){
             Help();
         }else if(strcmp(userInput, "version") == 0){
             Version();
-        }else if(strcmp(userInput, "dispdate") == 0){
+        }else if(strcmp(userInput, "date") == 0){
             DisplayDate();
         }else if(strcmp(userInput, "chdate") == 0){
             ChangeDate();
-        }else if(strcmp(userInput, "disptime") == 0){
+        }else if(strcmp(userInput, "time") == 0){
             DisplayTime();
         }else if(strcmp(userInput, "quit") == 0){
             TerminateTechOS();
@@ -56,11 +65,49 @@ void Version(){
 }
 
 void DisplayDate(){
-    printf("Display Date");
+    printf("%s %d, %d", getMonth(tm.tm_mon), tm.tm_mday, tm.tm_year + 1900);
+}
+
+char *getMonth(int month){
+    switch(month){
+        case 0: return "January"; break;
+        case 1: return "February"; break;
+        case 2: return "March"; break;
+        case 3: return "April"; break;
+        case 4: return "May"; break;
+        case 5: return "June"; break;
+        case 6: return "July"; break;
+        case 7: return "August"; break;
+        case 8: return "September"; break;
+        case 9: return "October"; break;
+        case 10: return "November"; break;
+        case 11: return "December"; break;
+    }
 }
 
 void ChangeDate(){
-    printf("Change Date");
+    size_t size = 20;
+    char *str = (char *)malloc(size * sizeof(char));
+    char delim[] = " -";
+    getline(&str, &size, stdin);
+    str = strtok(str, delim);
+
+    char *month, *day, *year;
+    char *vars[3];
+    int i = 0;
+    while(str != NULL && i < 3){
+        vars[i] = str;
+        str = strtok(NULL, delim);
+        i++;
+    }
+
+    month = vars[0];
+    day = vars[1];
+    year = vars[2];
+
+    tm.tm_mon = atoi(month) - 1;
+    tm.tm_mday = atoi(day);
+    tm.tm_year = atoi(year) - 1900;
 }
 
 void DisplayTime(){
